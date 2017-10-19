73477e7302182b314acbb6c2693fec35a22d2ea2
Junlapong L <junlapong@gmail.com>
2017-10-19 12:39:42 +0700

    add sslyze*.bat for proxy UAT and PROD

---
+++ sslyzeu.bat
@@ -0,0 +1,22 @@
+@echo off
+
+::------------------------------------------------------------------------
+:: PROXY ::
+:: SET PROXY="http://username:password@proxy-host:port"
+:: SET PROXY="http://127.0.0.1:3128"
+SET PROXY="http://192.168.3.112:8080"
+
+::------------------------------------------------------------------------
+
+sslyze_cli\sslyze_cli.exe ^
+--https_tunnel=%PROXY% ^
+--regular ^
+--hsts ^
+--ca_file=.\sslyze_cli\data\trust_stores\BAY_Root_CA.crt ^
+%1
+
+::--xml_out=%1.xml
+
+::------------------------------------------------------------------------
+
+@echo on
